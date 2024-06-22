a4e1022e9296f66f9fd6b9a6327a0b1b93497160
João <joaopedroolive@hotmail.com>
2024-06-22 00:07:36 -0300

    feat: Criando estrutura inicial

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "../../components/Header"
+
+const index = () => {
+  return (
+    <div>
+    <Header/>
+      Home aqui
+    </div>
+  )
+}
+
+export default index
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa";
+import Logo from "../assets/img/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="w-full bg-yellow-950 bg-opacity-5 backdrop-blur-md fixed z-50">
+      <nav className="flex items-center justify-between max-w-[1200px] mx-auto">
+        <Link to="/">
+          <img src={Logo} alt="Logo chef house" className="w-20 h-20"/>
+        </Link>
+
+        <Link to="/login" className="flex justify-center items-center gap-2">
+          <span>Logar / Criar</span>
+          <FaRegUser size={20}/>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
